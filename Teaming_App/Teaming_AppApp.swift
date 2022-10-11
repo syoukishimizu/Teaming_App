458f57f144d4a9df0a810214429f5101efe6d9c6
syoukishimizu <njacjjha.266727@gmail.com>
2022-10-11 11:59:37 +0900

    Initial Commit

---
+++ Teaming_App/Teaming_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Teaming_AppApp.swift
+//  Teaming_App
+//
+//  Created by 清水翔貴 on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct Teaming_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
